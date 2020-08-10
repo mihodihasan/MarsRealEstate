92f8a852480f51164713cbc4de992a46b2076f92
Lushan <mihodihasan@gmail.com>
2020-08-10 20:54:43 +0600

    price and isRent? value shown in details fragment

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.network.MarsProperty
 
 class DetailViewModel( marsProperty: MarsProperty,
@@ -15,5 +17,22 @@ class DetailViewModel( marsProperty: MarsProperty,
 
     init {
         _selectedProperty.value = marsProperty
+    }
+
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+                when (it.isRental) {
+                    true -> R.string.display_price_monthly_rental
+                    false -> R.string.display_price
+                }, it.price)
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+                app.applicationContext.getString(
+                        when (it.isRental) {
+                            true -> R.string.type_rent
+                            false -> R.string.type_sale
+                        }))
     }
 }
